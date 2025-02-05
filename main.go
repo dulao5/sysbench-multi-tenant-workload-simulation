e27867356d42bd064ee68742cf3baa80b838d525
dulao5 <dulao5@gmail.com>
2025-02-05 10:13:59 +0900

    add retry connect

--- main.go
+++ main.go
@@ -20,6 +20,13 @@ type TableInfo struct {
 	MaxK int
 }
 
+type SysbenchRow struct {
+	ID  int    `gorm:"primaryKey;autoIncrement"`
+	K   int    `gorm:"index:k_1"`
+	C   string `gorm:"size:120"`
+	Pad string `gorm:"size:60"`
+}
+
 func main() {
 	// Parse command-line flags
 	var (
@@ -96,11 +103,13 @@ func main() {
 		// Launch 'threadsPerDB' goroutines (long connections).
 		for i := 0; i < *threadsPerDB; i++ {
 			wg.Add(1)
+			time.Sleep(50 * time.Millisecond)
 			go func(conn *sql.DB, dbName string) {
 				defer wg.Done()
 				runWorker(conn, dbName, tables, *sleepAfterQueryMs, exitTime)
 			}(dbConn, dbName)
 		}
+		time.Sleep(50 * time.Millisecond)
 	}
 
 	// Wait for all goroutines to finish (though in this case they run indefinitely).
@@ -154,16 +163,46 @@ func prepareTables(bigTableNum, rowsPerBigTable int,
 	return tables
 }
 
+func makeActiveConn(db *sql.DB, dbName string, ctx context.Context) (*sql.Conn, error) {
+	log.Printf("[INFO] get conn for DB %s", dbName)
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		log.Printf("[ERROR] Failed to get conn for DB %s: %v", dbName, err)
+		return nil, err
+	}
+	err = conn.PingContext(ctx)
+	if err != nil {
+		log.Printf("[ERROR] Failed to ping conn for DB %s: %v", dbName, err)
+		return nil, err
+	}
+	return conn, nil
+}
+
+func retryMakeActiveConn(db *sql.DB, dbName string, ctx context.Context) (*sql.Conn, error) {
+	for {
+		conn, err := makeActiveConn(db, dbName, ctx)
+		if err != nil {
+			log.Printf("[WARNING] retry conn for DB %s: %v", dbName, err)
+			time.Sleep(50 * time.Millisecond)
+		} else {
+			return conn, nil
+		}
+	}
+}
+
 // runWorker gets one sql.Conn from the pool and continuously performs queries on that single connection.
 func runWorker(dbConn *sql.DB, dbName string, tables []TableInfo, sleepMs int, exitTime time.Time) {
 	// Get a dedicated connection from the pool.
 	ctx := context.Background()
-	conn, err := dbConn.Conn(ctx)
+	conn, err := retryMakeActiveConn(dbConn, dbName, ctx)
 	if err != nil {
 		log.Printf("[ERROR] Failed to get conn for DB %s: %v", dbName, err)
 		return
 	}
 	defer conn.Close()
+
+	// do a join select sql
+	_ = doJoinSelectRawDB(conn, ctx, 900)
 
 	// Infinite loop to continuously send queries.
 	for {
@@ -193,6 +232,7 @@ func runWorker(dbConn *sql.DB, dbName string, tables []TableInfo, sleepMs int, e
 		// If there's an error and it's not a "no rows" case, log it.
 		if err != nil && err != sql.ErrNoRows {
 			log.Printf("[ERROR] DB=%s table=%s k=%d query failed: %v", dbName, tableInfo.Name, kVal, err)
+			conn, _ = retryMakeActiveConn(dbConn, dbName, ctx)
 		} else {
 			// Optionally log or collect the duration metrics here.
 			// log.Printf("[INFO] DB=%s table=%s k=%d took=%v", dbName, tableInfo.Name, kVal, duration)
@@ -202,4 +242,55 @@ func runWorker(dbConn *sql.DB, dbName string, tables []TableInfo, sleepMs int, e
 		// Sleep to control QPS
 		time.Sleep(time.Duration(sleepMs) * time.Millisecond)
 	}
+}
+
+func doJoinSelectRawDB(conn *sql.Conn, ctx context.Context, maxId uint64) error {
+	// do Join select query
+	// table : sysbench.sbtest1
+	// id: 1~maxID
+	// limit 100
+	// left join : sbtest1 , sbtest2 , sbtest3 , sbtest4 on `id` colunm (as same value)
+	randID := uint64(rand.Int63n(int64(maxId)-100)) + 1
+	var result SysbenchRow
+
+	/*
+	       rows, err := conn.QueryContext(
+	           ctx,
+	           `select (sbtest1.id + sbtest5.id + sbtest6.id + sbtest7.id + sbtest8.id) as id, sbtest2.k as k, sbtest3.c as c, sbtest4.pad as pad
+	   from sbtest1
+	   LEFT JOIN sbtest2 ON sbtest1.id = sbtest2.id
+	   LEFT JOIN sbtest3 ON sbtest1.id = sbtest3.id
+	   LEFT JOIN sbtest4 ON sbtest1.id = sbtest4.id
+	   LEFT JOIN sbtest5 ON sbtest1.id = sbtest5.id
+	   LEFT JOIN sbtest6 ON sbtest1.id = sbtest6.id
+	   LEFT JOIN sbtest7 ON sbtest1.id = sbtest7.id
+	   LEFT JOIN sbtest8 ON sbtest1.id = sbtest8.id
+	   Where sbtest1.id >= ?
+	   limit 300`,
+	           randID,
+	       )
+	*/
+	rows, err := conn.QueryContext(
+		ctx,
+		`select (sbtest1.id) as id, sbtest2.k as k, sbtest3.c as c, sbtest4.pad as pad
+from sbtest1
+LEFT JOIN sbtest2 ON sbtest1.id = sbtest2.id
+LEFT JOIN sbtest3 ON sbtest1.id = sbtest3.id
+LEFT JOIN sbtest4 ON sbtest1.id = sbtest4.id
+Where sbtest1.id >= ?
+limit 100`,
+		randID,
+	)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// result
+	for rows.Next() {
+		if err := rows.Scan(&result.ID, &result.K, &result.C, &result.Pad); err != nil {
+			return err
+		}
+	}
+	return nil
 }
